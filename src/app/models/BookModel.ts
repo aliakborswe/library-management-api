81ad296f7117372beb25f32462bfdd1e4a2173d5
aliakborswe <aliakborswe@gmail.com>
2025-06-22 20:13:59 +0600

    book model done

---
+++ src/app/models/BookModel.ts
@@ -0,0 +1,50 @@
+import { model, Schema } from "mongoose"
+import { Genre, IBook } from "../interfaces/book.interface"
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: [true, "Author is required"],
+      trim: true,
+    },
+    genre: {
+      type: String,
+      required: [true, "Genre is required"],
+      enum: {
+        values: Object.values(Genre),
+        message: "Genre must be one of: FICTION, NON_FICTION, SCIENCE, HISTORY, BIOGRAPHY, FANTASY",
+      },
+    },
+    isbn: {
+      type: String,
+      required: [true, "ISBN is required"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    copies: {
+      type: Number,
+      required: [true, "Copies is required"],
+      min: [0, "Copies must be a non-negative number"],
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+) 
+
+export const Book = model<IBook>("Book", bookSchema)
